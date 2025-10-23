5f9d2757ae9612845c0979277613563ebd167b5e
timuryurdakan <yurdakantimur@gmail.com>
2025-10-23 14:06:30 +0300

    second commit

---
+++ s1+s2.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+ 
+    int main(){
+        int number1;
+        int number2;
+        int total=0;
+
+cout<<"\n\n\n\v\vplease enter two numbers\n\n\n"<< endl;
+    cin  >>number1>>number2;
+    total=number1+number2;
+    cout<<"result :"<<total<< endl;
+    return 0;
+    }
