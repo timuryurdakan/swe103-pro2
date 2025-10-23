296a59ca96a7a362f814b2bc43f062712734561f
timuryurdakan <yurdakantimur@gmail.com>
2025-10-23 14:04:01 +0300

    first commit

---
+++ basiccalculator.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int number1;
+    int number2;
+  cout<<"enter first number";
+  cin>>number1;
+  
+  cout<<"enter second number";
+  cin>>number2;
+
+  int sum
+ 
+
+   }
+   return 0;
+
+
+    
+}
