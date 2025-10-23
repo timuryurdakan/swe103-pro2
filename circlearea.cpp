8c5e41e079044df372525b8908e9c85f6520739a
timuryurdakan <yurdakantimur@gmail.com>
2025-10-23 14:08:25 +0300

    fourth

---
+++ circlearea.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main (){
+   
+    float radius;
+    const float PI=3.14;
+    float area;
+    
+cout<<"please enter radius of the circle:";
+cin>>radius;
+  area= PI*radius*radius;
+  
+cout<<"area of circle="<<area<<endl;
+
+return 0;
+
+}
+
