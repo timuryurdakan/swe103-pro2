33b630463224e3c128f47db4d055246fe9a66916
timuryurdakan <yurdakantimur@gmail.com>
2025-10-31 20:06:49 +0300

    first commit

---
+++ switchcase.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main (){
+    int number1;
+    int number2;
+    int total;
+    int islemtipi;
+
+    cout<<"lütfen islem tipini seciniz:";
+    cin>>islemtipi;
+    
+}
